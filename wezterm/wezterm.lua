0fcb010cc843c453553891b263987b84fc9d7ce1
Carlos Gomez <carlosandres0741@gmail.com>
2024-03-06 06:39:09 -0500

    feat: added wezterm config to windows

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,11 @@
 local wezterm = require 'wezterm'
+local user = os.getenv('USERNAME')
 
 return {
   color_scheme = "tokyonight",
   use_fancy_tab_bar = false,
-  default_cwd = "/home/carandev/dev/",
+  default_cwd = string.format("C:\\Users\\%s\\code", user),
+  default_prog = { 'powershell.exe' },
   hide_tab_bar_if_only_one_tab = true,
   tab_bar_at_bottom = true,
   window_padding = {
@@ -12,31 +14,64 @@ return {
     top = 0,
     bottom = 0
   },
-  font_size = 11,
-  font = wezterm.font 'CaskaydiaCove Nerd Font',
-  -- window_background_image = '/home/carandev/wallpapers/wallhaven-9mjoy1.png',
+  font_size = 13,
+  font = wezterm.font_with_fallback {
+    'CaskaydiaCove Nerd Font',
+    'Noto Color Emoji',
+    'DengXian'
+  },
+  window_background_image = string.format('C:\\Users\\%s\\Downloads\\Dark 2 Schein Wallpaper 1.png', user),
 
-  -- window_background_image_hsb = {
-  --   brightness = 0.05,
-  -- }
-  window_background_opacity = 0.8,
+  window_background_image_hsb = {
+    brightness = 0.05,
+  },
+
+  window_background_opacity = 0.9,
   text_background_opacity = 0.7,
   keys = {
-    { key = 'w', mods = 'CTRL|META', action = wezterm.action { CloseCurrentPane = { confirm = true } } },
     {
-      key = 'e',
+      key = 'w',
+      mods = 'CTRL|META',
+      action = wezterm.action { CloseCurrentPane =
+      {
+        confirm = true
+      }
+      }
+    },
+    {
+      key = 'n',
+      mods = 'CTRL|META',
+      action = wezterm.action { SpawnTab = {
+        DomainName = "WSL:Debian",
+      }
+      }
+    },
+    {
+      key = 'b',
       mods = 'CTRL|META',
       action = wezterm.action { SpawnCommandInNewTab = {
-        label = 'Ranger',
-        args = { 'bash', '-c ranger' },
-        cwd = '/home/carandev/dev/'
+        label = 'Backoffice',
+        args = { 'dotnet', 'watch', '--non-interactive' },
+        cwd = string.format('C:\\Users\\%s\\code\\Univer.CRM\\CRM.Backoffice.Web', user)
+      } }
+    },
+    {
+      key = 'a',
+      mods = 'CTRL|META',
+      action = wezterm.action { SpawnCommandInNewTab = {
+        label = 'API',
+        args = { 'dotnet', 'watch', '--non-interactive' },
+        cwd = string.format('C:\\Users\\%s\\code\\Univer.CRM\\CRM.API', user)
+      } }
+    },
+    {
+      key = 'u',
+      mods = 'CTRL|META',
+      action = wezterm.action { SpawnCommandInNewTab = {
+        label = 'UserPortal',
+        args = { 'dotnet', 'watch', '--non-interactive' },
+        cwd = string.format('C:\\Users\\%s\\code\\Univer.CRM\\CRM.UserPortal.Web', user)
       } }
     }
-  },
-  launch_menu = {
-    {
-      label = 'Explorador de archivos',
-      args = { 'ranger' },
-    },
   },
 }
